db256225b4cd6a3dd362a9f6bdcfeb39f2d638c9
Saai Sudarsanan D <saais@pop-os.localdomain>
2023-03-11 17:51:11 +0530

    read user input

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,32 @@ use std::{env, io::stdin};
     create
     read
 */
-
 fn main() {
     let args: Vec<String> = env::args().collect();
     let mode = &args[1];
 
-    print!("Please enter your username : ");
+    println!("Please enter your username : ");
     let mut username = String::new();
-    let username = stdin().read_line(&mut username).unwrap();
+    stdin().read_line(&mut username).unwrap();
 
-    print!("Please enter your service : ");
+    println!("Please enter your service : ");
     let mut service = String::new();
-    let service = stdin().read_line(&mut service).unwrap();
+    stdin().read_line(&mut service).unwrap();
 
-    print!("Please enter your master password : ");
+    println!("Please enter your master password : ");
     let mut mpass = String::new();
-    let mpass = stdin().read_line(&mut mpass).unwrap();
+    stdin().read_line(&mut mpass).unwrap();
 
     if mode == "create" {
-        println!("Create {} {} {}",username,service,mpass);
-    }
-    else if mode == "read" {
-        println!("Read {} {} {}",username,service,mpass);
-    }
-    else {
+        println!(
+            "Create {} {} {}",
+            username.trim(),
+            service.trim(),
+            mpass.trim()
+        );
+    } else if mode == "read" {
+        println!("Read {} {} {}", username, service, mpass);
+    } else {
         println!("Unknown Mode");
     }
 }
-
