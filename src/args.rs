b2f988e675a140953088d137580601de43caefb0
Saai Sudarsanan D <saais@pop-os.localdomain>
2023-03-12 12:12:42 +0530

    default len seed | rpassword

--- src/args.rs
+++ src/args.rs
@@ -1,18 +1,28 @@
-use clap:: {
-    Parser,
-};
+use clap::{ArgAction, Parser};
 
 #[derive(Debug, Parser)]
-#[clap(author,version,about)]
+#[clap(author, version, about)]
 pub struct RustPassArgs {
+    /// Use Uppercase
+    #[clap(long, short, action=ArgAction::SetTrue)]
+    pub uppercase: bool,
+    /// Use Lowercase
+    #[clap(long, short, action=ArgAction::SetTrue)]
+    pub lowercase: bool,
+    /// Use Special Characters
+    #[clap(long, short, action=ArgAction::SetTrue)]
+    pub special_chars: bool,
+    /// Use Digits
+    #[clap(long, short, action=ArgAction::SetTrue)]
+    pub digits: bool,
     /// Username
     pub username: String,
     /// Service Name
     pub service: String,
-    /// Master Password
-    pub mpass: String,
     /// Length of your Password
+    #[clap(default_value_t = 16)]
     pub len: i32,
     /// Seed Value
-    pub seed: i32,   
+    #[clap(default_value_t = 0)]
+    pub seed: i32,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
 mod args;
 use args::RustPassArgs;
 use clap::Parser;
+use rpassword;
 
 fn main() {
-    let args = RustPassArgs::parse(); 
+    let args = RustPassArgs::parse();
+    let mpass = rpassword::prompt_password(&String::from("Master Password: ")).unwrap();
 
-    println!("Username : {}",args.username);
-    println!("service : {}",args.service);
-    println!("mpass : {}",args.mpass);
-    println!("len : {}",args.len);
-    println!("seed : {}",args.seed);
+    println!("Username : {}", args.username);
+    println!("service : {}", args.service);
+    println!("mpass : {}", mpass);
+    println!("len : {}", args.len);
+    println!("seed : {}", args.seed);
 }
